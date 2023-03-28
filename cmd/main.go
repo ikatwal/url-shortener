1606b7a93f5a96f8059457e158921a2ab75969bd
ikatwal <teglanigiro@gmail.com>
2023-03-27 20:38:31 -0400

    time error fixed

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ikatwal/url-shortener/api/routes"
+	"github.com/joho/godotenv"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	router.GET("/:url", routes.ResolveURL)
+	router.POST("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading env file: %v", err)
+	}
+	router := gin.Default()
+	initializeRoutes(router)
+	log.Fatal(router.Run(os.Getenv("APP_PORT")))
+}
